d624f05fcddf1c83db5931acd17439e0ddb0d237
ethan jiang <ethan1.jiang@gmail.com>
2020-06-09 17:36:54 +0800

    init2

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,14 @@ class MyScaffold extends StatelessWidget {
             //child: Text('Hello, world! X '),
             child: MyButton(),
           ),
+          Expanded(
+            //child: Text('Hello, world! X '),
+            child: MyCounter0(),
+          ),
+          Expanded(
+            //child: Text('Hello, world! X '),
+            child: MyCounter1(),
+          ),
         ],
       ),
     );
@@ -95,6 +103,115 @@ class MyButton extends StatelessWidget {
     );
   }
 }
+
+class MyCounter0 extends StatefulWidget {
+  // This class is the configuration for the state. It holds the
+  // values (in this case nothing) provided by the parent and used
+  // by the build  method of the State. Fields in a Widget
+  // subclass are always marked "final".
+
+  @override
+  _CounterState0 createState() => _CounterState0();
+}
+
+class _CounterState0 extends State<MyCounter0> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If you change _counter without calling
+      // setState(), then the build method won't be called again,
+      // and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+  void _decrement() {
+    setState(() {
+      // This call to setState tells the Flutter framework that
+      // something has changed in this State, which causes it to rerun
+      // the build method below so that the display can reflect the
+      // updated values. If you change _counter without calling
+      // setState(), then the build method won't be called again,
+      // and so nothing would appear to happen.
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called,
+    // for instance, as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
+        ),
+        Text('Count: $_counter'),
+        RaisedButton(
+          onPressed: _decrement,
+          child: Text('Decrement'),
+        ),
+      ],
+    );
+  }
+}
+
+class MyCounterDisplay extends StatelessWidget {
+  MyCounterDisplay({this.count});
+
+  final int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Count: $count');
+  }
+}
+
+class CounterIncrementor extends StatelessWidget {
+  CounterIncrementor({this.onPressed});
+
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      onPressed: onPressed,
+      child: Text('Increment'),
+    );
+  }
+}
+
+class MyCounter1 extends StatefulWidget {
+  @override
+  _CounterState1 createState() => _CounterState1();
+}
+
+class _CounterState1 extends State<MyCounter1> {
+  int _counter = 0;
+
+  void _increment() {
+    setState(() {
+      ++_counter;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      CounterIncrementor(onPressed: _increment),
+      MyCounterDisplay(count: _counter),
+    ]);
+  }
+}
+
 void main() {
   runApp(MaterialApp(
     title: 'My app (invisible)', // used by the OS task switcher
