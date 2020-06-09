585e36080224945cb4bce8a33ea90e8679ea7f7d
ethan jiang <ethan1.jiang@gmail.com>
2020-06-09 17:17:48 +0800

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,103 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    Center(
-      child: childText(),
-    ),
-  );
+class MyAppBar extends StatelessWidget {
+  MyAppBar({this.title});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  pressMenu() {
+    print("pressMenu");
+  }
+
+  pressSearch() {
+    print("pressSearch");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56.0, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        // <Widget> is the type of items in the list.
+        children: <Widget>[
+          IconButton(
+            icon: Icon(Icons.menu),
+            tooltip: 'Navigation menu',
+            onPressed: pressMenu, // null disables the button
+          ),
+          // Expanded expands its child to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: pressSearch,
+          ),
+        ],
+      ),
+    );
+  }
 }
 
-Widget childText() {
-  return Text(
-    'Move Widget out.',
-    textDirection: TextDirection.ltr,
-  );
+class MyScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Material is a conceptual piece of paper on which the UI appears.
+    return Material(
+      // Column is a vertical, linear layout.
+      child: Column(
+        children: <Widget>[
+          MyAppBar(
+            title: Text(
+              'Example title X',
+              style: Theme.of(context).primaryTextTheme.headline6,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Text('Hello, world! X '),
+            ),
+          ),
+          Expanded(
+            //child: Text('Hello, world! X '),
+            child: MyButton(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+class MyButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        print('MyButton was tapped!');
+      },
+      child: Container(
+        height: 36.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.lightGreen[500],
+        ),
+        child: Center(
+          child: Text('Engage'),
+        ),
+      ),
+    );
+  }
+}
+void main() {
+  runApp(MaterialApp(
+    title: 'My app (invisible)', // used by the OS task switcher
+    home: MyScaffold(),
+  ));
 }
