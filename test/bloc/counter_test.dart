055c271057c0c201e25bc53749605d60c94c44bb
ethan jiang <ethan1.jiang@gmail.com>
2020-06-17 18:42:34 +0800

    add unit test now'

---
+++ test/bloc/counter_test.dart
@@ -0,0 +1,20 @@
+// Import the test package and Counter class
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutterapp2/bloc/counter.dart';
+
+void main() {
+  test('Counter value should be incremented', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 1);
+  });
+  test('Counter value should be incremented1', () {
+    final counter = Counter();
+
+    counter.increment();
+
+    expect(counter.value, 2);
+  });
+}
